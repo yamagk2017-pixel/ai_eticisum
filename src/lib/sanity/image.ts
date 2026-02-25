e38a268cf611423101ca55ff2d49e1db565a84b5
yamagk2017-pixel <yamagk2017@gmail.com>
2026-02-25 13:33:00 +0900

    Avoid internal Sanity image type import in helper

--- src/lib/sanity/image.ts
+++ src/lib/sanity/image.ts
@@ -1,10 +1,10 @@
 import imageUrlBuilder from "@sanity/image-url";
-import type {SanityImageSource} from "@sanity/image-url/lib/types/types";
 import {getSanityConfigValues} from "@/sanity/env";
 
 const {projectId, dataset} = getSanityConfigValues();
 
 const builder = imageUrlBuilder({projectId, dataset});
+type SanityImageSource = Parameters<typeof builder.image>[0];
 
 export function urlForSanityImage(source: SanityImageSource) {
   return builder.image(source);
